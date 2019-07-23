b60fdb2ec060a0c62fd7dcb029f3cfc057178015
Sebastian Moreno <46652667+sebastian-exe@users.noreply.github.com>
2019-07-22 23:03:54 -0500

    first commit
    re-check the time and feel free to change the ticker symbols so that it can work with whatever stock you need it to work with. Also, add functionality to include floats if necessary.

---
+++ stockcheck.py
@@ -0,0 +1,50 @@
+#adding time
+import time
+import smtplib
+#adding in the pip install from yahoo
+from yahoo_fin import stock_info as si
+
+#time successfully works, now will add a timer to check the price of these stocks
+#a few times a day and to notify me once the price reaches the targeted price
+
+
+print("apple")
+print(si.get_live_price("aapl"))
+
+print("amazon")
+print(si.get_live_price("amzn"))
+
+#define the price
+applePrice = si.get_live_price("aapl")
+
+#function to check the price
+
+
+def check_price():
+        if(applePrice = 218):
+            send_mail()
+
+#define the server function to send the sendMail
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    #encripts the connection
+    server.starttls()
+
+    server.login('sebamoreno17@gmail.com','#enter password for email#')
+
+    subject = 'target price reached.'
+    body = 'buy more stock!'
+    msg = f"Subject: {subject}\n\n{body}"
+
+    #the first parameter is the the recipient and the second parameter is the sender.
+    #The third parameter is the message
+    server.sendmail('sebamoreno17@gmail.com',
+    'sebamoreno17@gmail.com',
+    msg)
+    print("email has been sent!")
+    server.quit()
+
+
+while(True):
+    check_price()
+    time.sleep(60)
